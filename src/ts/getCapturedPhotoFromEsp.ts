ce7397533173e510dd1fbcda30643a76858c77e0
Patrick <onyebuchiudeh1@yahoo.com>
2021-02-21 19:53:53 +0100

    binding frontend to backend

--- src/ts/getCapturedPhotoFromEsp.ts
+++ src/ts/getCapturedPhotoFromEsp.ts
@@ -1,132 +1,111 @@
 
 // Fetches image from esp and saves as an img.src in the index.html
- async function fetchCapturedPixFromEsp() {
-  //let prom =await fetch('http://192.168.2.124/capture')
+async function fetchCapturedPixFromEsp() {
+   //let prom =await fetch('http://192.168.2.124/capture')
 
-  let response = await fetch('http://192.168.2.124/saved-photo')
-  if (response.status === 200) {
-    let data = await response.blob();
-    // handle data:  convert img to string using Base64, then save to localstorage and set as src. TODO: save to path,
-    let reader = new FileReader();
-    await reader.readAsDataURL(data);
-    let newImgUrlFromBase64data : string
-    reader.onloadend = async function() {
-      let base64data = reader.result; //returns file as encoded string
-      newImgUrlFromBase64data =   String(base64data)//dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-      await createImageTag("imgFromEsp", newImgUrlFromBase64data)
-      console.log("imageTag created to display image from ESP")
-    }
-  }
+   let response = await fetch('http://localhost:8080/api/automate/auto') //('http://192.168.2.124/saved-photo')
+   if (response.status === 200) {
+     let data = await response.blob();
+     // handle data:  convert img to string using Base64, then save to localstorage and set as src. TODO: save to path,
+     let reader = new FileReader();
+     await reader.readAsDataURL(data);
 
+     reader.onloadend = async function () {
+       let base64data = reader.result; //returns file as encoded string
+       let newImgUrlFromBase64data = String(base64data)//dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+       let srcItem = localStorage.getItem("srcUrl")
+       if(!srcItem){
+         localStorage.setItem("srcUrl", newImgUrlFromBase64data)
+       }
+       else localStorage.setItem("srcUrl2", newImgUrlFromBase64data)
+       //await createImageTag("imgFromEsp", newImgUrlFromBase64data)
+     }
+     let istImg = document.getElementById("istImg_id") as HTMLImageElement
+     if(localStorage.getItem("srcUrl") && localStorage.getItem("srcUrl2")){
+       istImg.src =  <string>localStorage.getItem("srcUrl2")
+     }
+     else istImg.src =  <string>localStorage.getItem("srcUrl")
 
-  /*  //Check if the picture is already in localstorage and get it otherwise get from ESP alert("capture returned no image")
-    let newImgSrcFromStore = localStorage.getItem("imgRcvdi")!
-    if(newImgSrcFromStore !== null && newImgSrcFromStore !== "data:"){
-      await createImageTag("savedImg", newImgSrcFromStore)
-      console.log("image is from Local Storage")
-      return;
-    }
+     istImg.style.width = "100%"
+     istImg.style.height = "100%"
+     //istImg.src =  <string>localStorage.getItem("srcUrl")
+     console.log("imageTag created to display image from ESP")
+     istImg.style.transform = "rotate(" + 90 + "deg)";
 
-    let reader = new FileReader();
-    await reader.readAsDataURL(data);
+     /*istImg.style.width = "100%"
+     istImg.style.height = "100%"
+     istImg.style.objectFit = "contain"
+     istImg.style.transform = "rotate(" + 90 + "deg)"
+     if(localStorage.getItem("srcUrl") && localStorage.getItem("srcUrl2"))
+       istImg.src =  <string>localStorage.getItem("srcUrl2")
+     else istImg.src =  <string>localStorage.getItem("srcUrl")*/
+   }
 
-    reader.onloadend = async function() {
-      let base64data = reader.result; //returns file as encoded string
-      let newImgUrlFromBase64data =   String(base64data)//dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-      localStorage.setItem("imgRcvd", newImgUrlFromBase64data);//String.replace(/^data:image\/(png|jpg);base64,/, "rcvd")
-   } */
-    /*if (response.status === 200) {
-      await createImageTag("imgId", "src/img/fetched/icon.jpg")
-    }*/
-    /*  //get img from localstorage and save to dir and pass the path as src
-    let newImgSrc = localStorage.getItem("imgRcvd")!
-    if(newImgSrc === null || newImgSrc == "data:"){
-      console.log("No saved pix in ESP")
-    }
-    else {
-      await createImageTag("imgFromEsp", newImgSrc)
-      console.log("imageTag created to display image from ESP")}
-  }*/
+ }
+
+function fetchCapturedPixFromLocalStorage(img: HTMLImageElement, id: string) {
+    img.style.objectFit = "contain"
+    img.style.width = "84%"
+    img.style.height = "auto"
+    img.src =  <string>localStorage.getItem(id)
+    console.log("src from Localstorage is set")
 }
-/*
-fetchPixFromEsp().then(r => {
-  console.log('fetched')
-}); */
 
-/*Creates an img tag based on if the id is savedImg or imgFromEsp*/ // create the tag first and fetch the src later
+
+let nBtn = document.getElementById("getImgEsp")!
+nBtn.addEventListener("click", async () => {
+  await fetchCapturedPixFromEsp()
+    .then(r => console.log("image from dsp is gotten"));
+})
+
+let storeBtn = document.getElementById("getImgStore")!
+let imgToBeUpdated = document.getElementById("2ndImg_id") as HTMLImageElement
+let imgId = "srcUrl"
+storeBtn.addEventListener("click", async () => {
+   if(!(imgToBeUpdated.src == localStorage.getItem(imgId))){
+     console.log("2ndImg_id no such Id exists in the doc")
+     fetchCapturedPixFromLocalStorage(imgToBeUpdated, imgId)
+   }
+   else{
+     imgId = "srcUrl2"
+     imgToBeUpdated = document.getElementById("3rdImg_id") as HTMLImageElement
+     fetchCapturedPixFromLocalStorage(imgToBeUpdated, imgId)
+     console.log("3rdImg_id no such Id exists in the doc")
+   }
+})
+
+let newHeight
+let newWidth
+function resizeImg(img: HTMLImageElement, maxWidth: number, maxHeight: number){
+  if (img.width > img.height) {
+    newHeight = img.height * (maxWidth / img.width);
+    newWidth = maxWidth;
+  } else {
+    newWidth = img.width * (maxHeight / img.height);
+    newHeight = maxHeight;
+  }
+  return img;
+}
+
+/*Creates an img tag based on if the id is savedImg or imgFromEsp // create the tag first and fetch the src later
  async function createImageTag(id: string, src: string){
   let imgTag = document.createElement("img")
   imgTag.id = id;
-  imgTag. style. width = '400px';
-  imgTag.style. height = 'auto';
-  imgTag.style.margin = '20px';
+  imgTag. style. width = '33%x';
+  imgTag.style. height = '330px';
   imgTag.src = src;
-  document.body.append(imgTag)
-}
+  document.body.append(imgTag);
+  return imgTag
+}*/
 
-/*Creates an img tag based on if the id is savedImg or imgFromEsp*/
-async function createButton(btnName: string, id: string){
+/*Creates an img tag based on if the id is savedImg or imgFromEsp
+ function createButton(btnName: string, id: string){
   let newBtn = document.createElement("button")
   newBtn.id = id;
   newBtn.innerHTML = btnName;
-  document.body.append(newBtn)
-}
-
-createButton("GetCapturedPixFromESP", "capturedImg").then(r => console.log("getPicture btn created"))
-
-let nBtn = document.getElementById("capturedImg")!
-nBtn.addEventListener("click", async () => {
-  await fetchCapturedPixFromEsp()
-    .then(r => console.log("captured"));
-})
-
-//location.reload();
-//alert('reloaded')
- /* async function getBase64Image(img: any) {
-    let canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-
-    let ctx = canvas.getContext("2d")!;
-    ctx.drawImage(img, 0, 0);
-
-    let dataURL = canvas.toDataURL("src/img/img/");
-
-    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-  }
-
-let bannerImage = document.getElementById('bannerImg');
-let imgData = getBase64Image(bannerImage);
-localStorage.setItem("imgData", String(imgData));
-
-
-let dataImage = localStorage.getItem('imgData');
-let bannerImg = (document.getElementById('tableBanner') as HTMLImageElement)
-bannerImg.src = "data:image/png;base64," + dataImage;
-*/
-
-
-/*
-  async function getJpg(){
-    let response = await fetch('src/img/pix.jpg')
-    .then(response => response.blob())
-      .then(images => {
-        // Then create a local URL for that image and print it //save it here src/img/fetched
-        let outside = URL.createObjectURL(images)
-        console.log("out: ", outside)
-      })
-    let elem = (document.getElementById('imgId') as HTMLImageElement);
-    elem.src = "src/img/fetched/icon.jpg"
-    console.log("src from getJpg set")
-  }
-
-  getJpg().then(r => {
-    console.log('image arrived')
-  });
-
-*/
-
-
+  //document.body.append(newBtn)
+   return newBtn
+}*/
 
 
 
